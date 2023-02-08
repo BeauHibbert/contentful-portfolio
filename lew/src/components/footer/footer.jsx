b4d8f05623488ad8c8af0997155658b0c5334132
Beau Hibbert <beauhibbert789@gmail.com>
2023-02-08 15:58:55 -0800

    footer

---
+++ lew/src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+
+
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Grayscale Creative | 2022</p>
+      <a href="https://github.com/beauhibbert">Site built by Beau Hibbert</a>
+    </footer>
+  )
+}
+
+export default Footer;
