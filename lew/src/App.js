6875811838fa5ceddbc52cbc06d067764a168393
Beau Hibbert <beauhibbert789@gmail.com>
2023-02-07 19:25:20 -0800

    header setup

--- lew/src/App.js
+++ lew/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import Footer from './components/footer.jsx';
+import SingleProjectPage from './pages/single-project/single-project.jsx';
+import AboutMePage from './pages/about-me/about-me.jsx';
+import ContactPage from './pages/contact-page/contact-page.jsx';
+import ProjectPage from './pages/projects/projects.jsx';
+import HomePage from './pages/HomePage/HomePage';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return ( 
+    <Router>
+      <Header/>
+      <Routes>
+        <Route 
+          path="/"
+          element={<HomePage/>}
+        />
+        <Route 
+          path="portfolio/:slug"
+          element={<SingleProjectPage/>}
+        />
+        <Route 
+          path="portfolio"
+          element={<AboutMePage/>}
+        />
+        <Route 
+          path="services"
+          element={<ProjectPage/>}
+        />
+        <Route 
+          path="contact"
+          element={<ContactPage/>}
+        />
+        <Route 
+          path="*"
+          element={<HomePage/>}
+        />
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
